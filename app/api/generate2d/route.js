4c4cd867c35f888b5d9bc16bd3ce8e18c5974046
contrabandinteractive <77306606+contrabandinteractive@users.noreply.github.com>
2024-07-06 16:03:04 -0500

    Add files via upload

---
+++ app/api/generate2d/route.js
@@ -0,0 +1,93 @@
+export const maxDuration = 300;
+import { NextResponse } from 'next/server'
+import { Client } from "@gradio/client";
+
+
+export async function POST(request) {
+
+    let body = await request.json();
+    let userInput = body.prompt;
+
+  
+    const apiUrl = process.env.NEXT_PUBLIC_SDAPI;
+
+    const thePrompt = {
+      prompt: "A highly detailed, realistic, hand drawn sketch of "+userInput+". A single item and/or person, entirely within the frame. No color, black and white.",
+      negative_prompt: "multiple characters, cut-off",
+      styles: ["realistic"],
+      seed: -1,
+      subseed: -1,
+      subseed_strength: 0,
+      seed_resize_from_h: -1,
+      seed_resize_from_w: -1,
+      sampler_name: "Euler",
+      batch_size: 3,
+      n_iter: 1,
+      steps: 50,
+      cfg_scale: 7,
+      width: 512,
+      height: 512,
+      restore_faces: true,
+      tiling: false,
+      do_not_save_samples: false,
+      do_not_save_grid: false,
+      eta: 0,
+      denoising_strength: 0,
+      s_min_uncond: 0,
+      s_churn: 0,
+      s_tmax: 0,
+      s_tmin: 0,
+      s_noise: 0,
+      override_settings: {},
+      override_settings_restore_afterwards: true,
+      refiner_checkpoint: "",
+      refiner_switch_at: 0,
+      disable_extra_networks: false,
+      comments: {},
+      enable_hr: false,
+      firstphase_width: 0,
+      firstphase_height: 0,
+      hr_scale: 2,
+      hr_upscaler: "",
+      hr_second_pass_steps: 0,
+      hr_resize_x: 0,
+      hr_resize_y: 0,
+      hr_checkpoint_name: "",
+      hr_sampler_name: "",
+      hr_prompt: "",
+      hr_negative_prompt: "",
+      sampler_index: "Euler",
+      script_name: "",
+      script_args: [],
+      send_images: true,
+      save_images: false,
+      alwayson_scripts: {}
+    }
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(thePrompt),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log(result);
+
+    return NextResponse.json({ result }, { status: 200 });
+
+
+
+
+} catch (error) {
+  console.error('Error:', error);
+  return NextResponse.json({ message: 'error' }, { status: 500 });
+}
+
+}
---
+++ app/page.js
@@ -0,0 +1,278 @@
+'use client'
+import Image from "next/image";
+import React, { useState, useEffect } from 'react';
+import { motion } from "framer-motion"
+import Web3 from 'web3';
+import { useRouter } from 'next/navigation'
+
+
+
+export default function Home() {
+  const router = useRouter();
+  const [inputText, setInputText] = useState('');
+  const [imageUrl1, setImageUrl1] = useState('');
+  const [imageUrl2, setImageUrl2] = useState('');
+  const [imageUrl3, setImageUrl3] = useState('');
+  const [chosenImg, setChosenImg] = useState('');
+  const [nftID, setNftID] = useState(null)
+  const [step1Done, setStep1Done] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [account, setAccount] = useState(null);
+  const [web3, setWeb3] = useState(null);
+
+
+  function generateRandom5DigitID() {
+    const theID = Math.floor(10000 + Math.random() * 90000);
+    setNftID(theID);
+    return theID;
+  }
+
+  const handleSelectChange = (event) => {
+    setChosenImg(event.target.value);
+    console.log(chosenImg);
+    setStep1Done(true);
+  };
+
+  const goToMarketplace = () => {
+    router.push('/marketplace');
+  };
+
+
+  const handleGenerateImage = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch('/api/generate2d', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ prompt: inputText }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch');
+      }
+
+      const data = await response.json();
+      const generatedImageUrl1 = data.result.images[0]; 
+      const generatedImageUrl2 = data.result.images[1]; 
+      const generatedImageUrl3 = data.result.images[2]; 
+      //console.log(data.result.images[0]);
+
+      setImageUrl1('data:image/png;base64,'+generatedImageUrl1);
+      setImageUrl2('data:image/png;base64,'+generatedImageUrl2);
+      setImageUrl3('data:image/png;base64,'+generatedImageUrl3);
+      setChosenImg(1);
+    } catch (error) {
+      console.error('Error generating image:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (window.ethereum) {
+      const web3Instance = new Web3(window.ethereum);
+      setWeb3(web3Instance);
+    } else {
+      console.warn('Please install MetaMask!');
+    }
+  }, []);
+
+  const connectMetamask = async () => {
+    if (web3) {
+      try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setAccount(accounts[0]);
+      } catch (error) {
+        console.error('Error connecting to MetaMask', error);
+      }
+    }
+  };
+
+  const mintNFT = async () => {
+    if (!web3 || !account) return;
+
+    const contractAddress = process.env.NEXT_PUBLIC_CONTRACT;
+    const contractABI = [{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"address","name":"owner","type":"address"}],"name":"ERC721IncorrectOwner","type":"error"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ERC721InsufficientApproval","type":"error"},{"inputs":[{"internalType":"address","name":"approver","type":"address"}],"name":"ERC721InvalidApprover","type":"error"},{"inputs":[{"internalType":"address","name":"operator","type":"address"}],"name":"ERC721InvalidOperator","type":"error"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"ERC721InvalidOwner","type":"error"},{"inputs":[{"internalType":"address","name":"receiver","type":"address"}],"name":"ERC721InvalidReceiver","type":"error"},{"inputs":[{"internalType":"address","name":"sender","type":"address"}],"name":"ERC721InvalidSender","type":"error"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ERC721NonexistentToken","type":"error"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"OwnableInvalidOwner","type":"error"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"OwnableUnauthorizedAccount","type":"error"},{"anonymous":!1,"inputs":[{"indexed":!0,"internalType":"address","name":"owner","type":"address"},{"indexed":!0,"internalType":"address","name":"approved","type":"address"},{"indexed":!0,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":!1,"inputs":[{"indexed":!0,"internalType":"address","name":"owner","type":"address"},{"indexed":!0,"internalType":"address","name":"operator","type":"address"},{"indexed":!1,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":!1,"inputs":[{"indexed":!1,"internalType":"uint256","name":"_fromTokenId","type":"uint256"},{"indexed":!1,"internalType":"uint256","name":"_toTokenId","type":"uint256"}],"name":"BatchMetadataUpdate","type":"event"},{"anonymous":!1,"inputs":[{"indexed":!1,"internalType":"uint256","name":"_tokenId","type":"uint256"}],"name":"MetadataUpdate","type":"event"},{"anonymous":!1,"inputs":[{"indexed":!0,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":!0,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":!1,"inputs":[{"indexed":!0,"internalType":"address","name":"from","type":"address"},{"indexed":!0,"internalType":"address","name":"to","type":"address"},{"indexed":!0,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"burn","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"string","name":"uri","type":"string"}],"name":"safeMint","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"initialOwner","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"ROYALTY_PERCENTAGE","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"}];
+
+    const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+    try {
+      const myNFTID = generateRandom5DigitID();
+      await contract.methods.safeMint(account, myNFTID,"data:image/png;base64,"+chosenImg).send({ from: account });
+      console.log('NFT Minted');
+      setNftID(myNFTID);
+    } catch (error) {
+      console.error('Error minting NFT', error);
+    }
+  };
+
+  return (
+    
+    <div className="min-h-screen bg-gray-900 text-white">
+      
+      <div className="container mx-auto p-4 w-full">
+        <div className="flex justify-center items-center pb-[20px] pt-[60px]">
+        <img src="logo.png" className="max-w-[500px] w-[100%]" />
+        </div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+        <div className="max-w-lg mx-auto bg-gray-800 p-6 rounded-lg shadow-lg mb-[40px]">
+          <h2 className="text-3xl font-bold text-center mb-4">Step 1</h2>
+          <textarea
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            placeholder="Describe what you want to create..."
+            rows={4}
+            className="w-full bg-gray-700 text-white p-2 rounded-lg mb-4 resize-none"
+          />
+          <button
+            onClick={handleGenerateImage}
+            className="w-full bg-[#c940e6] hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline"
+            disabled={isLoading}
+          >
+            {isLoading ? 'Generating...' : 'Generate Images'}
+          </button>
+          <div className="flex space-x-4">
+          {imageUrl1 && (
+            <div className="mt-4 flex-1">
+              <img src={imageUrl1} alt="Generated Image" className="rounded-lg shadow-lg" />
+              <p>Sketch #1</p>
+              <a
+                href={imageUrl1}
+                download="generated_image.png"
+                className="block mt-4 bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg text-center"
+              >
+                Download
+              </a>
+            </div>
+          )}
+          {imageUrl2 && (
+            <div className="mt-4 flex-1">
+              <img src={imageUrl2} alt="Generated Image" className="rounded-lg shadow-lg" />
+              <p>Sketch #2</p>
+              <a
+                href={imageUrl2}
+                download="generated_image.png"
+                className="block mt-4 bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg text-center"
+              >
+                Download
+              </a>
+            </div>
+          )}
+          {imageUrl3 && (
+            <div className="mt-4 flex-1">
+              <img src={imageUrl3} alt="Generated Image" className="rounded-lg shadow-lg" />
+              <p>Sketch #3</p>
+              <a
+                href={imageUrl3}
+                download="generated_image.png"
+                className="block mt-4 bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg text-center"
+              >
+                Download
+              </a>
+            </div>
+          )}
+          </div>
+          
+          {imageUrl3 && (
+          <div className="w-64">
+            <label htmlFor="image-select" className="block text-white mb-2">
+              Choose an Image:
+            </label>
+            <select
+              id="image-select"
+              value={chosenImg}
+              onChange={handleSelectChange}
+              className="w-full px-3 py-2 bg-gray-700 text-white rounded-lg"
+            >
+              <option value="" disabled>Select an image</option>
+              <option value={imageUrl1}>Sketch 1</option>
+              <option value={imageUrl2}>Sketch 2</option>
+              <option value={imageUrl3}>Sketch 3</option>
+            </select>
+
+         </div>
+          )}
+
+        </div>
+        </motion.div>
+        
+        {imageUrl3 && step1Done && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+          <div className="mx-auto bg-gray-800 p-6 rounded-lg shadow-lg">
+            <h2 className="text-3xl font-bold text-center mb-4">Step 2</h2>
+            <iframe src={process.env.NEXT_PUBLIC_SKETCHTO3D_URL} width="100%" height="800" />
+          </div>
+        </motion.div>
+        )}
+        
+        {imageUrl3 && step1Done && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+
+          <div className="w-full max-w-3xl mx-auto bg-gray-800 p-6 rounded-lg shadow-lg mt-6">
+            <h2 className="text-3xl font-bold text-center mb-4">Step 3: Connect to MetaMask</h2>
+            <button
+              onClick={connectMetamask}
+              className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline"
+            >
+              Connect MetaMask
+            </button>
+            {account && (
+              <div className="mt-4">
+                <p>Connected account: {account}</p>
+                <button
+                  onClick={mintNFT}
+                  className="w-full bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline mt-4"
+                >
+                  Mint NFT
+                </button>
+              </div>
+            )}
+          </div>
+        </motion.div>
+        )}
+
+        {nftID && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+
+          <div className="w-full max-w-3xl mx-auto bg-gray-800 p-6 rounded-lg shadow-lg mt-6 mb-[100px]">
+            <h2 className="text-3xl font-bold text-center mb-4">Step 4: Success!</h2>
+            <p> Your NFT has been minted on Theta Testnet!</p>
+            
+            <p>Contract: {process.env.NEXT_PUBLIC_CONTRACT}</p>
+            <p className="text-xl underline">ID: 242444</p>
+            <p>You can use this ID to import into your wallet.</p>
+
+            <p className="pt-5">Original NFT creators receive 5% royalties on secondary sales.</p>
+            <button
+                  onClick={goToMarketplace}
+                  className="w-full bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline mt-4"
+                >
+                  View Marketplace
+                </button>
+          </div>
+        </motion.div>
+        )}
+
+      </div>
+    </div>
+  );
+}
